4df771f5764e52824e47110540496ed116565fa5
a92038839-collab <a92038839@gmail.com>
2026-09-21 12:25:22 +0500

    Update auction_kb.py

--- auction_kb.py
+++ auction_kb.py
@@ -1,1 +1,20 @@
- 
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+def auction_list_kb(auctions):
+    """Aktiv auksionlar ro'yxati uchun tugmalar"""
+    kb = InlineKeyboardMarkup(row_width=1)
+    for auc in auctions:
+        kb.add(InlineKeyboardButton(
+            text=f"📦 {auc[1]} | 💰 {auc[5]} tanga",
+            callback_data=f"view_auc_{auc[0]}"
+        ))
+    return kb
+
+def bid_kb(auction_id):
+    """Bitta auksion uchun tugma"""
+    kb = InlineKeyboardMarkup()
+    kb.add(InlineKeyboardButton(
+        text="💸 Taklif berish",
+        callback_data=f"bid_{auction_id}"
+    ))
+    return kb 
